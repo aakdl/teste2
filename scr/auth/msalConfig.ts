a7c6d6dd279d4dc0cc52890d1918cd884f7f5832
aakdl <148289397+aakdl@users.noreply.github.com>
2025-12-03 22:50:53 -0300

    Add files via upload

--- scr/auth/msalConfig.ts
+++ scr/auth/msalConfig.ts
@@ -1,1 +1,13 @@
+import { Configuration } from "@azure/msal-browser";
 
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: "dc784c34-6594-4f76-b709-21c23fe35a84",
+    authority: "https://login.microsoftonline.com/26bef5f1-7958-4594-8cbf-19b3b090d072",
+    redirectUri: "http://localhost:3000"
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false
+  }
+};
